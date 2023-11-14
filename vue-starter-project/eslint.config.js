b4d920dd482fb6eb6dff1b2978c1e9356bfb1af2
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2023-11-14 20:49:16 +0100

    Add files via upload

--- vue-starter-project/eslint.config.js
+++ vue-starter-project/eslint.config.js
@@ -4,8 +4,22 @@ export default antfu({
   vue: true,
   typescript: false,
 }, {
+  files: ['**/*.vue'],
   rules: {
+    'vue/singleline-html-element-content-newline': 'error',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+}, {
+  rules: {
+    'curly': 'off',
     'no-console': 'off',
-    'style/semi': ['error', 'always'],
+    'style/semi': ['error', 'always']
   },
 });
